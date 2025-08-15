2007cd31e0d61d2c9deb51a233b20cd483dca557
Prasanna Gadal <gadalprasanna5@gmail.com>
2025-08-15 21:57:14 +0545

    User informations storedin database

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,14 +1,13 @@
 
-;
 import { pgTable, uuid, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
 
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
-    clerId: text("clerk_id").unique().notNull(),
+    clerkId: text("clerk_id").unique().notNull(),
     name: text("name").notNull(),
 
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull()
-}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerId)]);
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
