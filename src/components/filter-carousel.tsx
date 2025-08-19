9be5389089caf1b7f694c58b1be04830185c9364
Prasanna Gadal <gadalprasanna5@gmail.com>
2025-08-19 22:24:33 +0545

    All badge in filter-categorie bug on not getting selected fixed

--- src/components/filter-carousel.tsx
+++ src/components/filter-carousel.tsx
@@ -1,11 +1,11 @@
 "use client"
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-    CarouselApi
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  CarouselApi
 } from "@/components/ui/carousel"
 import { Badge } from "./ui/badge";
 import { cn } from "@/lib/utils";
@@ -13,105 +13,102 @@ import { useState, useEffect } from "react";
 import { Skeleton } from "./ui/skeleton";
 
 interface FilterCarouselProps {
-    value?: string | null;
-    isLoading?: boolean;
-    onSelect?: (value: string | null) => void;
-    data: {
-        value: string;
-        label: string;
-    }[];
+  value?: string | null;
+  isLoading?: boolean;
+  onSelect?: (value: string | null) => void;
+  data: {
+    value: string;
+    label: string;
+  }[];
 }
 
 export const FilterCarousel = ({
-    value,
-    onSelect,
-    isLoading,
-    data
+  value,
+  onSelect,
+  isLoading,
+  data
 }: FilterCarouselProps) => {
 
-    const [api, setApi] = useState<CarouselApi>()
-    const [current, setCurrent] = useState(0);
-    const [count, setCount] = useState(0);
+  const [api, setApi] = useState<CarouselApi>()
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        if (!api) return;
+  useEffect(() => {
+    if (!api) return;
 
-        setCount(api.scrollSnapList().length);
-        setCurrent(api.selectedScrollSnap() + 1);
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
 
-        api.on("select", () => {
-            setCurrent(api.selectedScrollSnap() + 1);
-        });
-    }, [api]);
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
-    return (
-        <div className="relative w-full px-2 ">
-            {/* Left Fade */}
-            <div
-                className={cn(
-                    "absolute left-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-r from-white to-transparent pointer-events-none",
-                    current === 1 && "hidden"
-                )}
-            />
+  return (
+    <div className="relative w-full px-2 ">
+      {/* Left Fade */}
+      <div
+        className={cn(
+          "absolute left-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-r from-white to-transparent pointer-events-none",
+          current === 1 && "hidden"
+        )}
+      />
 
-            <Carousel
-                opts={{
-                    align: "start",
-                    dragFree: true,
-                }}
-                setApi={setApi}
-                className="w-full px-11"
-            >
-                <CarouselContent>
-                    {!isLoading && (
-                        <CarouselItem
-                            onClick={() => onSelect?.(null)}
-                            className="pl-3 basis-auto"
-                        >
-                            <Badge
-                                variant={value === null ? "default" : "secondary"}
-                                className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
-                                onClick={() => onSelect?.(null)}
-                            >
-                                All
-                            </Badge>
-                        </CarouselItem>
-                    )}
+      <Carousel
+        opts={{
+          align: "start",
+          dragFree: true,
+        }}
+        setApi={setApi}
+        className="w-full px-11"
+      >
+        <CarouselContent>
+          {!isLoading && (
+            <CarouselItem className="pl-3 basis-auto">
+              <Badge
+                variant={value === null || value === undefined ? "default" : "secondary"}
+                className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+                onClick={() => onSelect?.(null)}
+              >
+                All
+              </Badge>
+            </CarouselItem>
+          )}
+          
+          {isLoading && 
+            Array.from({length : 14}).map((_, index) => (
+              <CarouselItem key={index} className="pl-3 basis-auto">
+                <Skeleton className="rounded-lg px-3 py-1 h-full text-sm w-[100px] font-semibold">
+                  &nbsp;
+                </Skeleton>
+              </CarouselItem>
+            ))}
 
-                    {isLoading &&
-                        Array.from({ length: 14 }).map((_, index) => (
-                            <CarouselItem key={index} className=" pl-3 basis-auto" >
-                                <Skeleton className="rounded-lg px-3 py-1
-                     h-full text-sm w-[100px] font-semibold">
-                                    &nbsp;
-                                </Skeleton>
-                            </CarouselItem>
-                        ))}
-                    {!isLoading &&
-                        data.map((item) => (
-                            <CarouselItem key={item.value} className="pl-3 basis-auto" onClick={() => onSelect?.(item.value)}>
-                                <Badge
-                                    variant={value === item.value ? "default" : "secondary"}
-                                    className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
-                                    onClick={() => onSelect?.(item.value)}
-                                >
-                                    {item.label}
-                                </Badge>
-                            </CarouselItem>
-                        ))}
-                </CarouselContent>
+          {!isLoading &&
+            data.map((item) => (
+              <CarouselItem key={item.value} className="pl-3 basis-auto">
+                <Badge
+                  variant={value === item.value ? "default" : "secondary"}
+                  className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
+                  onClick={() => onSelect?.(item.value)}
+                >
+                  {item.label}
+                </Badge>
+              </CarouselItem>
+            ))}
+        </CarouselContent>
 
-                <CarouselPrevious className="left-0 z-20" />
-                <CarouselNext className="right-0 z-20" />
-            </Carousel>
+        <CarouselPrevious className="left-0 z-20" />
+        <CarouselNext className="right-0 z-20" />
+      </Carousel>
 
-            {/* Right Fade */}
-            <div
-                className={cn(
-                    "absolute right-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-l from-white to-transparent pointer-events-none",
-                    current === count && "hidden"
-                )}
-            />
-        </div>
-    );
+      {/* Right Fade */}
+      <div
+        className={cn(
+          "absolute right-12 top-0 bottom-0 w-12 z-10 bg-gradient-to-l from-white to-transparent pointer-events-none",
+          current === count && "hidden"
+        )}
+      />
+    </div>
+  );
 };
--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -5,6 +5,7 @@ import { trpc } from "@/trpc/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 import { FilterCarousel } from "@/components/filter-carousel";
+import { useRouter } from "next/navigation";
 
 interface CategorySectionProps {
     categoryId?: string;
@@ -12,7 +13,7 @@ interface CategorySectionProps {
 
  const CategoriesSection = ({categoryId} : CategorySectionProps) => {
     return(
-        <Suspense fallback={<p>Loading</p>}> 
+        <Suspense fallback={<FilterCarousel data={[]} onSelect={() => {}}/>}> 
             <ErrorBoundary fallback={<p>Error...</p>}>
                 <CategoriesSectionSuspense categoryId={categoryId}/>
             </ErrorBoundary>
@@ -23,15 +24,26 @@ interface CategorySectionProps {
 
 const CategoriesSectionSuspense = ({categoryId} : CategorySectionProps) => {
     const [categories] = trpc.categories.getMany.useSuspenseQuery();
-
+    const router = useRouter();
     const data = categories.map((category)=> ({
         value: category.id,
         label: category.name,
     }))
+
+    const onSelect = (value: string | null) => {
+        const url = new URL(window.location.href);
+
+        if(value) {
+            url.searchParams.set("categoryId", value);
+        }else{
+            url.searchParams.delete("categoryId");
+        }
+
+        router.push(url.toString());
+    }
     return(
         <>
-            <FilterCarousel value={categoryId} data={data}/>
-            
+            <FilterCarousel onSelect= {onSelect}value={categoryId} data={data}/>
         </>
     )
 }
